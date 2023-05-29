478b8155d04bcb220bdef5fc91ea4d0adddfec98
ferrari219@nate.com <ferrari219@nate.com>
2023-05-30 08:46:31 +0900

    front: 초기 셋팅

---
+++ front/pages/_app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+
+const app = ({ Component }) => {
+  return (
+    <>
+      <Head>
+        <title>KioskSimulate</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="description" content="KioskSimulate" />
+        <meta name="author" content="Graham" />
+        <meta keyword="KioskSimulate" />
+      </Head>
+      <Component />
+    </>
+  );
+};
+app.proptypes = {
+  Component: PropTypes.elementType.isRequired,
+};
+
+export default app;
